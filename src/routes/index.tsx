3d25cfbc387a2a82a3cdf4edd20637d251eba74c
Phyce <ronaldas@mechanic.ink>
2024-04-25 03:09:16 +0200

    View stats link no longer opens a new tab

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -94,7 +94,7 @@ export default component$(() => {
                                         <td class="py-1 px-3">{truncateString(plugin.description, 100, '...')}</td>
                                         <td class="py-1 px-3">{formatDate(plugin.updated_on)}</td>
                                         <td class="py-1 px-3">
-                                            <a href={"/" + plugin.name} target="_blank" class="text-organge-600 hover:underline text-orange-500">View Stats</a>
+                                            <a href={"/" + plugin.name} class="text-organge-600 hover:underline text-orange-500">View Stats</a>
                                         </td>
                                     </tr>
                                 ))
